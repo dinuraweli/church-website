c526075d4c1a76dd339be993d55663eff5f36a1f
dinuraweli <dinurawelikala@gmail.com>
2025-10-15 09:14:58 +0530

    updated about page

--- main.js
+++ main.js
@@ -42,3 +42,13 @@ document.addEventListener('keydown', (event) => {
   }
 });
 
+
+  if (window.netlifyIdentity) {
+    window.netlifyIdentity.on("init", user => {
+      if (!user) {
+        window.netlifyIdentity.on("login", () => {
+          document.location.href = "/admin/";
+        });
+      }
+    });
+  }
