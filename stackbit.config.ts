cf347da5b1e032130df874a4dce05c8d9c2e25ab
dinuraweli <dinurawelikala@gmail.com>
2025-10-16 18:30:05 +0530

    updated about page

---
+++ stackbit.config.ts
@@ -0,0 +1,21 @@
+import { defineStackbitConfig } from "@stackbit/types";
+
+export default defineStackbitConfig({
+  stackbitVersion: "~0.6.0",
+  nodeVersion: "18",
+
+  contentSources: [
+    {
+      name: "gitcms",
+      type: "git",
+      models: [
+        {
+          name: "page",
+          type: "page",
+          filePathPattern: "index.html",
+          urlPath: "/",
+        },
+      ],
+    },
+  ],
+});
